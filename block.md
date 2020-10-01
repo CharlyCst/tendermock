cdc8c5c1f4fd976945689399fe2aae8b43dd6624
CharlyCst <castes.ch@gmail.com>
2020-10-01 17:26:44 +0200

    WIP

---
+++ block.md
@@ -0,0 +1,49 @@
+# Blocks
+
+Just a small file to help me wrap my head around Tendermint blocks.
+
+## The block structure
+
+```rs
+pub struct Block {
+    pub header: Header,
+    pub data: transaction::Data,
+    pub evidence: evidence::Data,
+    pub last_commit: Option<Commit>,
+}
+```
+
+Let's build the first block: we'll put the `Commit` aside for now.
+
+### Data
+
+The data is a vector of `Transaction`s, those transactions are simply arbitrary bytes of data too (`Vec<u8>`).
+
+### Evidence
+
+A simple list of evidences, let's use an empty list.
+
+### Header
+
+
+The hard part...
+
+```
+pub struct Header {
+    pub version:              Version,
+    pub chain_id:             chain::Id,
+    pub height:               block::Height,
+    pub time:                 Time,
+    pub last_block_id:        Option<block::Id>,
+    pub last_commit_hash:     Option<Hash>,
+    pub data_hash:            Option<Hash>,
+    pub validators_hash:      Hash,
+    pub next_validators_hash: Hash,
+    pub consensus_hash:       Hash,
+    pub app_hash:             Vec<u8>,
+    pub last_results_hash:    Option<Hash>,
+    pub evidence_hash:        Option<Hash>,
+    pub proposer_address:     account::Id,
+}
+```
+
--- src/bin/server.rs
+++ src/bin/server.rs
@@ -7,6 +7,18 @@ use tendermint_rpc::endpoint::{
     validators::Request as ValidatorsRequest,
 };
 
+use tendermint::account;
+use tendermint::block;
+use tendermint::chain;
+use tendermint::block::header::{Header, Version};
+use tendermint::consensus;
+use tendermint::evidence;
+use tendermint::public_key;
+use tendermint::time;
+use tendermint::validator;
+use tendermint::vote;
+use tendermint::abci;
+
 #[rpc(server)]
 pub trait Rpc {
     #[rpc(name = "commit", params = "raw")]
@@ -38,7 +50,7 @@ fn main() {
     io.extend_with(server.to_delegate());
 
     let server = ServerBuilder::new(io)
-        .start_http(&"127.0.0.1:3030".parse().unwrap())
+        .start_http(&"127.0.0.1:26657".parse().unwrap())
         .expect("Unable to start RPC server");
 
     server.wait();
@@ -55,4 +67,74 @@ where
         Params::Map(val) => serde_json::Value::Object(val),
     };
     serde_json::from_value(params).map_err(|err| JsonError::invalid_params(err.to_string()))
+}
+
+/// Returns the `Info` about a factice validator.
+fn get_validator() -> validator::Info {
+    let key = public_key::Ed25519::from_bytes(&[
+        215, 90, 152, 1, 130, 177, 10, 183, 213, 75, 254, 211, 201, 100, 7, 58, 14, 225, 114, 243,
+        218, 166, 35, 37, 175, 2, 26, 104, 247, 7, 81, 26,
+    ])
+    .unwrap();
+    let voting_power = vote::Power::new(10);
+
+    validator::Info::new(public_key::PublicKey::Ed25519(key), voting_power)
+}
+
+/// Returns the `Params` of a factice concensus.
+fn _get_concensus() -> consensus::Params {
+    let block = block::Size {
+        max_bytes: 2048,
+        max_gas: 64,
+    };
+    let evidence = evidence::Params {
+        max_age_num_blocks: 5,
+        max_age_duration: evidence::Duration(std::time::Duration::new(5, 0)),
+    };
+    let validator = consensus::params::ValidatorParams {
+        pub_key_types: vec![public_key::Algorithm::Ed25519],
+    };
+
+    consensus::Params {
+        block,
+        evidence,
+        validator,
+    }
+}
+
+/// TODO: build a block
+fn _get_bloc() -> block::Block {
+    let version = Version { block: 0, app: 0 };
+    let chain_id = chain::Id::from("zephyr");
+    let height = block::Height::from(1);
+    let time = time::Time::now();
+    let validators_hash = validator::Set::new(vec![get_validator()]).hash();
+    let next_validators_hash = validators_hash.clone();
+    let consensus_hash = validators_hash.clone();
+    let app_hash = vec![b'a'; 10];
+    let proposer_address = account::Id::new([b'a'; 20]);
+
+    let header = Header {
+        version,
+        chain_id,
+        height,
+        time,
+        last_block_id: None,
+        last_commit_hash: None,
+        data_hash: None,
+        validators_hash,
+        next_validators_hash,
+        consensus_hash,
+        app_hash,
+        last_results_hash: None,
+        evidence_hash: None,
+        proposer_address,
+    };
+
+    block::Block {
+        header,
+        data: abci::transaction::Data::new(vec![]),
+        evidence: evidence::Data::new(vec![]),
+        last_commit: None,
+    }
 }
